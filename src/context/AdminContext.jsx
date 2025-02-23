095bfe7f4a0436fca8c5d4aa2cb62a335bdc6a19
FazleRahmanEvan <evanshakib26@gmail.com>
2025-02-23 14:05:21 +0600

    first commit

---
+++ src/context/AdminContext.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import { createContext } from "react";
+
+export const AdminContext = createContext();
+
+const AdminContextProvider = (props) => {
+  const value = {};
+  return (
+    <AdminContext.Provider value={value}>
+      {props.children}
+    </AdminContext.Provider>
+  );
+};
+
+export default AdminContextProvider;
